9ea9758e84d1055f4e5591fe4e7541ead502644a
Maxim Bordeaux <grossgameboy@gmail.com>
2021-06-16 00:51:09 +0500

    Initial commit

--- src/User.kt
+++ src/User.kt
@@ -1,3 +1,11 @@
 class User (var name:String? = null, var cash:Int? = null)
 
+open class Coffee(var size:String, var price:Int){
+    init {
+        if (size == "большой"){
+            price += 20
+        }
+    }
+}
 
+class Capuccino:Coffee("lol",100)
